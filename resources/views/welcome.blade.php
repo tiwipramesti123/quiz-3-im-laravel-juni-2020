ce43dc52c20bdcd8e40f02064db94d1207631358
Pramesti Ayupratiwi <pramestiayupratiwi10@gmail.com>
2020-07-05 15:17:03 +0700

    tugas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Sanbercode
+                    <a href={{asset('/erd.png'>)}};''
                 </div>
 
                 <div class="links">
